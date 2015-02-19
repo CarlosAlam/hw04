7d2b90436108cb4238837748c323b4ba08a13f00
Haruso <upci.pe1501@gmail.com>
2015-02-19 00:06:58 -0600

    Tarea resuelta
    La tarea es version compilable

--- hw_01_id_27.cpp
+++ hw_01_id_27.cpp
@@ -1,19 +1,37 @@
-// Author:
-// Date:
-// Version:
-
 /*
-Homework DD
-this assignment  ...
+Author:   Juan Pueblo
+Date:     today
+Version:  1.0
+
+  Homework 04
+  Media de tres variables
+	
 */
+
 
 #include <iostream>
 
 int main ()
 {
-  // TODO your code here!!!!
+  int a=0, b=0, c=0, mint=0;
+  double x=0, y=0, z=0, mdou=0;
+  char chara, charb, charc, mchar;
 
-  std::cout<<"Hello world!!" <<std::endl;
+  std::cout<<"\nEste programa calcula la media de tres tipos de datos dados por el usuario "<<std::endl;
+  std::cout<<"\nEscribe tres enteros: \n"; 
+  std::cin>>a; std::cin>>b; std::cin>>c;
+  std::cout<<"\nEscriba tres numeros double: \n";
+  std::cin>>x; std::cin>>y; std::cin>>z;
+  std::cout<<"\nEscriba tres caracter: \n";
+  std::cin>>chara; std::cin>>charb; std::cin>>charc;
+   
+   mint=(a+b+c)/3;
+   mdou=(x+y+z)/3;
+   mchar=(chara+charb+charc)/3;
 
+  std::cout<<"\nLa media de los int es: "<<mint <<std::endl;
+  std::cout<<"\nLa media de los double es: "<<mdou <<std::endl;
+  std::cout<<"\nLa media de los char es: "<<mchar <<std::endl;
+  
   return 0;
 }
